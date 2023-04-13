017817a76f0562e207b4bcae3830b7b1690899df
bumgyu <soul1902@naver.com>
2023-04-13 16:50:30 +0900

    메인페이지공통(footer,header) index.html

---
+++ mycgv/src/main/java/com/example/mycgv/MycgvApplication.java
@@ -0,0 +1,13 @@
+package com.example.mycgv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MycgvApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MycgvApplication.class, args);
+    }
+
+}
