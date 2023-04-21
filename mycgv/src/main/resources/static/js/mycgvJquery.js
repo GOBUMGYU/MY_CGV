c80203eec1c58c7a0f8c60c5648f283ab1cfc863
bumgyu <soul1902@naver.com>
2023-04-21 18:52:06 +0900

    1. join.html - <script src="//t1.daumcdn.net/mapjsapi/bundle/postcode/prod/postcode.v2.js"></script> 다음 우편 API 추가 2. mycgvJquery.js(클라이언트) - 셀렉트 박스 값 변경 이벤트 - 비밀번호, 비밀번호 확인 비교 검사

--- mycgv/src/main/resources/static/js/mycgvJquery.js
+++ mycgv/src/main/resources/static/js/mycgvJquery.js
@@ -1,9 +1,10 @@
 $(document).ready(function() {
+
+
     /**
      * 회원가입 폼 유효성 체크
      */
     $("#btnJoin").click(()=>{
-
         if($("#id").val() == ""){
             alert("아이디를 입력해주세요");
             $("#id").focus();
@@ -61,6 +62,58 @@ $(document).ready(function() {
             //서버전송
             joinForm.submit();
         }
+    });
 
+
+    /**
+     * 이메일 주소 선택
+     * 셀렉트 박스의 값이 변경될 때, 이벤트 핸들러 호출
+     * 이벤트 핸들러 내부에서 if-else조건문을 통해 셀렉트 박스의 값을 판별하고, 적절한 처리를 수행
+     * 셀렉트 박스의 값이 default라면 이메일 주소를 선택해주세요라는 경고 창을 띄우고, 이메일 주소 입력란과 도메인 주소 입력란을 초기화
+     * 셀렉트 박스의 값이 self라면 도메인 입력란을 초기화하고, 이메일 주소 입력란에 포커스를 준다.
+     */
+    $("#email3").change(()=>{
+       if($("email3").val() == "default") {
+           alert("이메일 주소를 선택해주세요");
+           $("#email3").focus();
+           $("#email2").val("");
+       }else if($("#email3").val() == "self") {
+           $("#email2").val("").focus();
+       }else {
+           $("#email2").val($("#email3").val());
+       }
+    });
+
+    /**
+     * 비밀번호, 비밀번호 확인 비교
+     */
+    $("#cpass").on("blur",()=>{
+        if($("#pass").val() != "" && $("#cpass").val() != ""){
+            if($("#pass").val() == $("#cpass").val()){
+                $("#passCheckMsg").text("*비밀번호가 동일합니다.")
+                    .css("color","blue").css("font-size","12px");
+
+            }else{
+                $("#passCheckMsg").text("*비밀번호가 동일하지 않습니다. 다시 입력해주세요.")
+                    .css("color","red").css("font-size","12px");
+
+                $("#cpass").val("");
+                $("#pass").val("").focus();
+            }
+        }//else{} --> 회원가입 폼의 유효성 체크로 진행됨
+    });
+
+
+    /**
+     * 회원가입 - 주소찾기
+     */
+    $("#btnSearchAddr").click(function(){
+        new daum.Postcode({
+            oncomplete: function(data) {
+                $("#zonecode").val(data.zonecode);
+                $("#addr1").val(data.address);
+                $("#addr2").focus();
+            }
+        }).open();
     });
 }); //ready Function
