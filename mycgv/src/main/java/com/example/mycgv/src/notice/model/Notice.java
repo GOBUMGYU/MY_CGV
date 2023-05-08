5a2525592e22a2266fc99748a4efb77554b50a33
bumgyu <soul1902@naver.com>
2023-05-08 22:15:35 +0900

    공지사항 도메인

---
+++ mycgv/src/main/java/com/example/mycgv/src/notice/model/Notice.java
@@ -0,0 +1,27 @@
+package com.example.mycgv.src.notice.model;
+
+
+import jdk.jfr.Timestamp;
+import lombok.*;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.web.multipart.MultipartFile;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Notice {
+
+    private Long nid;
+    private String ntitle;
+    private String ncontent;
+
+    private String nfile;
+    private String nsfile;
+    private int nhits;
+    @Timestamp
+    private String createAt;
+    @UpdateTimestamp
+    private String updateAt;
+    private MultipartFile file1;
+}
