d285618d361cbd75d0a4cb41e417baef13123654
bumgyu <soul1902@naver.com>
2023-05-10 22:24:10 +0900

    1. AdminController /admin/movie /admin/movie/regist /admin/movie/list (Json) /admin/movie/content  추가

--- mycgv/src/main/java/com/example/mycgv/src/admin/AdminController.java
+++ mycgv/src/main/java/com/example/mycgv/src/admin/AdminController.java
@@ -2,14 +2,23 @@ package com.example.mycgv.src.admin;
 
 import com.example.mycgv.src.FileServiceImpl;
 import com.example.mycgv.src.PageServiceImpl;
+import com.example.mycgv.src.movie.Movie;
+import com.example.mycgv.src.movie.MovieService;
 import com.example.mycgv.src.notice.NoticeService;
 import com.example.mycgv.src.notice.model.Notice;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServlet;
@@ -24,6 +33,7 @@ import java.util.Map;
 public class AdminController {
 
     private final NoticeService noticeService;
+    private final MovieService movieService;
     private final PageServiceImpl pageService;
     private final FileServiceImpl fileService;
 
@@ -37,9 +47,9 @@ public class AdminController {
         ModelAndView mv = new ModelAndView();
 
         Map<String, Integer> param = pageService.getPageResult(rpage, "notice", noticeService);
-        List<Notice> list = noticeService.noticeList(param.get("startCount"),param.get("endCount"));
+        List<Notice> list = noticeService.noticeList(param.get("startCount"), param.get("endCount"));
 
-        mv.addObject("list",list);
+        mv.addObject("list", list);
         mv.addObject("dbCount", param.get("dbCount"));
         mv.addObject("rpage", param.get("rpage"));
         mv.addObject("pageSize", param.get("pageSize"));
@@ -61,10 +71,10 @@ public class AdminController {
         vo = fileService.fileCheck(vo);
         int result = noticeService.createNotice(vo);
 
-        if(result == 1) {
+        if (result == 1) {
             fileService.fileSave(vo, request);
             mv.setViewName("redirect:/admin/notice");
-        }else {
+        } else {
             mv.setViewName("error_page");
         }
         return mv;
@@ -102,10 +112,10 @@ public class AdminController {
 
         int result = noticeService.update(vo);
 
-        if(result == 1) {
+        if (result == 1) {
             fileService.updateFileSave(vo, request, old_filename);
             mv.setViewName("redirect:/admin/notice");
-        }else {
+        } else {
             mv.setViewName("error_page");
         }
         return mv;
@@ -127,13 +137,92 @@ public class AdminController {
         Notice vo = noticeService.content(nid);
         int result = noticeService.delete(nid);
 
-        if(result == 1) {
+        if (result == 1) {
             fileService.fileDelete(vo, request);
             mv.setViewName("redirect:/admin/notice");
-        }else {
+        } else {
             mv.setViewName("error_page");
         }
         return mv;
     }
 
+
+    @GetMapping("/movie")
+    public ModelAndView adminMovieListForm(String rpage) {
+        ModelAndView mv = new ModelAndView();
+
+        Map<String, Integer> param = pageService.getPageResult(rpage, "movie", movieService);
+        List<Movie> list = movieService.movieList(param.get("startCount"), param.get("endCount"));
+
+        mv.addObject("list", list);
+        mv.addObject("dbCount", param.get("dbCount"));
+        mv.addObject("rpage", param.get("rpage"));
+        mv.addObject("pageSize", param.get("pageSize"));
+        mv.setViewName("/admin/admin_movie/adminMovieList");
+
+        return mv;
+    }
+
+    @GetMapping("/movie/regist")
+    public ModelAndView adminMovieRegistForm() {
+        ModelAndView mv = new ModelAndView();
+
+        mv.setViewName("/admin/admin_movie/adminMovieRegist");
+        return mv;
+    }
+
+    @PostMapping("/movie/regist")
+    public ModelAndView adminMovieRegist(Movie movie, HttpServletRequest request) throws Exception {
+        ModelAndView mv = new ModelAndView();
+
+        movie = fileService.multiFileCheck(movie);
+
+        int result = movieService.createMovie(movie);
+
+        if (result == 1) {
+            Long mid = Long.valueOf(movieService.getMid());
+
+            movie.setMid(mid);
+            int result2 = movieService.getInsertFile(movie);
+
+            if (result2 == 1) {
+                fileService.multiFileSave(movie, request);
+            }
+        }
+        mv.setViewName("redirect:/admin/movie");
+        return mv;
+    }
+
+    @ResponseBody
+    @GetMapping("/movie/list")
+    public String movieListJson() throws JsonProcessingException {
+        List<Movie> list = movieService.selectList();
+
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode rootNode = mapper.createObjectNode();
+        ArrayNode listNode = mapper.createArrayNode();
+
+        for(Movie vo:list) {
+            ObjectNode movieNode = mapper.createObjectNode();
+            movieNode.put("mid", vo.getMid());
+            movieNode.put("mname", vo.getMname());
+            movieNode.put("msfile", vo.getMsfile1());
+            listNode.add(movieNode);
+        }
+
+        ((ObjectNode) rootNode).set("list", listNode);
+        String jsonString = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(rootNode);
+        System.out.println(jsonString);
+        return jsonString;
+    }
+
+    @GetMapping("/movie/content")
+    public ModelAndView adminMovieContent(Long mid) {
+        ModelAndView mv = new ModelAndView();
+
+        Movie vo = movieService.content(mid);
+        mv.addObject("vo", vo);
+        mv.setViewName("/admin/admin_movie/adminMovieContent");
+        return mv;
+    }
 }
