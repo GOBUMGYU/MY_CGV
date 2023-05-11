b26ff5369b027e8ebd1c4eae9de01b7899edca18
bumgyu <soul1902@naver.com>
2023-05-11 22:36:33 +0900

    UserController 추가

---
+++ mycgv/src/main/java/com/example/mycgv/src/user/UserController.java
@@ -0,0 +1,91 @@
+package com.example.mycgv.src.user;
+
+import com.example.mycgv.src.SessionVO;
+import com.example.mycgv.src.user.model.PostUserReq;
+import com.example.mycgv.src.user.model.User;
+import com.example.mycgv.utils.JwtService;
+import lombok.AllArgsConstructor;
+import org.hibernate.Session;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
+
+@AllArgsConstructor
+@RequestMapping("/users")
+@Controller
+public class UserController {
+
+    private final UserServiceImpl userService;
+    private final JwtService jwtService;
+
+    /**
+     * 아이디 중복 처리 체크
+     */
+    @ResponseBody
+    @GetMapping("/id_check")
+    public String idCheck(String id) {
+        int result = userService.getIdCheck(id);
+        return String.valueOf(result);
+    }
+
+    /**
+     * 회원가입 Form
+     */
+    @GetMapping("/new")
+    public String JoinForm() {
+        return "join/join";
+    }
+
+    /**
+     * 회원가입 처리
+     */
+    @PostMapping("/new")
+    public ModelAndView createUser(@Valid PostUserReq postUserReq) {
+        ModelAndView mv = new ModelAndView();
+        int result = userService.createUser(postUserReq);
+        if(result == 1) {
+            mv.addObject("join_result", "ok");
+            mv.setViewName("redirect:/users/login");
+        }else  {
+            mv.setViewName("errorPage");
+        }
+        return mv;
+    }
+
+    /**
+     * login Form
+     */
+    @GetMapping("/login")
+    public ModelAndView LoginForm(String auth) {
+        ModelAndView mv = new ModelAndView();
+        mv.addObject("auth", auth);
+        mv.setViewName("/login/login");
+        return mv;
+    }
+
+    /**
+     * 로그인 처리
+     */
+    @PostMapping("/login")
+    public ModelAndView login(PostUserReq vo, HttpSession session) {
+        ModelAndView mv = new ModelAndView();
+        SessionVO svo = userService.getLoginResult(vo);
+
+        if(svo != null) {
+            if(svo.getLoginresult() == 1) {
+                session.setAttribute("svo", svo);
+                mv.addObject("login_result", "ok");
+                mv.setViewName("/index");
+            }
+        }else {
+            mv.addObject("login_result", "fail");
+            mv.setViewName("/login/login");
+        }
+        return mv;
+    }
+
+}
