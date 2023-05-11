4a45c86d84ec51a382da296dd4b481cc1332c997
bumgyu <soul1902@naver.com>
2023-05-11 22:35:53 +0900

    SessionVO추가

---
+++ mycgv/src/main/java/com/example/mycgv/src/SessionVO.java
@@ -0,0 +1,12 @@
+package com.example.mycgv.src;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class SessionVO {
+    private int loginresult;
+    private String id;
+    private String name;
+}
