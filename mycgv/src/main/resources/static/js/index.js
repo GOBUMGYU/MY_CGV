0457f9cef37362dd0ec27975e9475ea8f501f7a5
bumgyu <soul1902@naver.com>
2023-05-11 22:34:41 +0900

    1. index(홈).html 공지사항 리스트 Aajx로 가져오기

---
+++ mycgv/src/main/resources/static/js/index.js
@@ -0,0 +1,58 @@
+$(document).ready(function () {
+    initNoticeList(1);
+    movieList();
+
+    function initNoticeList(rpage) {
+
+        $.ajax({
+            url : "notice/list?rpage="+rpage,
+            success : function(result) {
+                let notice = JSON.parse(result);
+
+                let output = "<div class='notice_style'><h3>공지사항</h3>";
+                output += "<div>&nbsp;&nbsp;<span id='pre'>pre</span>&nbsp;&nbsp;|";
+                output += "&nbsp;&nbsp;<span id='next'>next</span>&nbsp;&nbsp;|";
+                output += "&nbsp;&nbsp;<span id='more'>more</span></div>";
+                output += "<ul class='notice_list'>";
+                for(data of notice.list){
+                    output += "<li>";
+                    output += "<span>"+ data.nid +"</span>";
+                    output += "<span>"+ data.ntitle +"</span>";
+                    output += "<span>"+ data.createAt +"</span>";
+                    output += "<span>"+ data.nhits +"</span>";
+                    output += "</li>";
+                }
+
+                output += "</ul></div>";
+
+
+                //3. 생성된 Dynamic HTML 코드를 해당위치에 출력
+                $("div.notice_style").remove();
+                $("div.s2_article > div").after(output);
+
+                //pre, next 버튼 활성화
+                if(rpage >= 1 && rpage < notice.pageCount ){
+                    $("#pre").css("visibility","hidden");
+                    $("#next").css("color","red");
+                }else{
+                    $("#pre").css("color","red");
+                //    $("#next").css("visibility","hidden");
+                }
+
+                //4. Dynamic HTML 코드에서 이벤트 처리 진행
+                $("#pre").click(function(){
+                    initNoticeList(rpage-1);
+                });
+
+                $("#next").click(function(){
+                    initNoticeList(rpage+1);
+                });
+
+                $("#more").click(function(){
+                    $(location).attr("href","notice/list");
+                });
+
+            }//success
+        });//ajax
+    }//initNoticeList
+});
